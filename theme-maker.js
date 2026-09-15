e43bfdccda50504d6b55a304958a2494463f5198
e4493089-cmyk <e4493089@gmail.com>
2026-09-16 07:31:38 +0900

    Sync theme 3.50.5

--- theme-maker.js
+++ theme-maker.js
@@ -1,4 +1,4 @@
-const BASE_THEME_VERSION = '3.50.4';
+const BASE_THEME_VERSION = '3.50.5';
 const BASE_THEME_URL = 'https://raw.githubusercontent.com/e4493089-cmyk/zeta-userscripts/main/zeta-kakaotalk-theme.user.js';
 
 const $ = selector => document.querySelector(selector);
